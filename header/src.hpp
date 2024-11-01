eb1134302cd1aa16b617c1bc7269aec616122f3a
lleyden <lleyden2@kent.edu>
2024-11-01 16:00:27 -0400

    Create src.hpp

---
+++ header/src.hpp
@@ -0,0 +1,19 @@
+/*  Lukas Leyden
+    CS 33211
+    Programming Assignment #1
+    */
+
+
+#ifndef SRC_HPP
+#define SRC_HPP
+
+#include <stdio.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <iostream>
+#include <fcntl.h>
+#include <sys/mman.h>
+
+int main(int, char**);
+
+#endif
