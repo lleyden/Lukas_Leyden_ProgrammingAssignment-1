578a1143717bcab432e068f36f01472d47dbbf16
lleyden <lleyden2@kent.edu>
2024-11-01 16:00:52 -0400

    Create memorybuff.hpp

---
+++ header/memorybuff.hpp
@@ -0,0 +1,27 @@
+/*  Lukas Leyden
+    CS 33211
+    Programming Assignment #1
+    */
+
+
+#ifndef MEMORYBUFF_HPP
+#define  MEMORYBUFF_HPP
+
+#include <stdlib.h>
+#include <sys/shm.h>
+#include <sys/ipc.h>
+#include <semaphore.h>
+
+#define TABLE_SIZE 2
+
+struct sharedMem {
+    int table[TABLE_SIZE];
+    int in;
+    int out;
+
+    sem_t mutex;
+    sem_t empty;
+    sem_t full;
+};
+
+#endif
